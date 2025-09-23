73e450664f4a3154ae8e2c0bb089623f5d772d27
Simone Scarduzio <scarduzio@gmail.com>
2025-09-23 13:11:03 +0200

    lint

--- src/deltaglider/_version.py
+++ src/deltaglider/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "0.1.3.post0"
-__version_tuple__ = version_tuple = (0, 1, 3, "post0")
+__version__ = version = '0.1.3.post0'
+__version_tuple__ = version_tuple = (0, 1, 3, 'post0')
 
-__commit_id__ = commit_id = "gb78899907"
+__commit_id__ = commit_id = 'gc30609efd'
